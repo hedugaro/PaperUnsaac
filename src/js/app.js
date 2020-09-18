9475c72005bd81ec4d91464b891ee7248d5a2622
Hector Ugarte <hedugaro@hotmail.com>
2020-09-17 22:19:34 -0500

    Estoy haciendo commit

---
+++ src/js/app.js
@@ -0,0 +1,102 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('ERC20Basic.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      var ERC20BasicArtifact = data;
+      App.contracts.ERC20Basic = TruffleContract(ERC20BasicArtifact);
+
+      // Set the provider for our contract.
+      App.contracts.ERC20Basic.setProvider(App.web3Provider);
+
+      // Use our contract to retieve and mark the adopted pets.
+      return App.getBalances();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '#transferButton', App.handleTransfer);
+  },
+
+  handleTransfer: function(event) {
+    event.preventDefault();
+
+    var amount = parseInt($('#TTTransferAmount').val());
+    var toAddress = $('#TTTransferAddress').val();
+
+    console.log('Transfer ' + amount + ' TT to ' + toAddress);
+
+    var ERC20BasicInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.ERC20Basic.deployed().then(function(instance) {
+        ERC20BasicInstance = instance;
+
+        return ERC20BasicInstance.transfer(toAddress, amount, {from: account, gas: 100000});
+      }).then(function(result) {
+        alert('Transfer Successful!');
+        return App.getBalances();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  getBalances: function() {
+    console.log('Getting balances...');
+
+    var ERC20BasicInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.ERC20Basic.deployed().then(function(instance) {
+        ERC20BasicInstance = instance;
+        return ERC20BasicInstance.balanceOf(account);
+      }).then(function(result) {
+        balance = result.c[0];
+        $('#TTBalance').text(balance);
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  }
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
